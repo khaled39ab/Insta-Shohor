36060582e1e96a1e4afdff0ab2d4d825652486d8
khaled39ab <khaled39ab@gmail.com>
2022-05-09 20:33:35 +0600

    fixed username & user comment

--- js/app.js
+++ js/app.js
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -134,12 +134,12 @@ const createPost = (post) => {
 };
 
 const showPosts = (posts) => {
-    const productsContainer = document.getElementById( "posts" );
-    productsContainer.innerHTML = "";
+    const postsContainer = document.getElementById( "posts" );
+    postsContainer.innerHTML = "";
 
     posts.forEach((post) => {
         const div = createPost(post);
-        productsContainer.appendChild(div);
+        postsContainer.appendChild(div);
     });
 };
 
