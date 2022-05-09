2de78ded3b94b7733b2f33ac92c47a3fca933ef8
khaled39ab <khaled39ab@gmail.com>
2022-05-09 21:55:38 +0600

    fixed user's photo

--- js/app.js
+++ js/app.js
@@ -52,7 +52,6 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
-  const userPic = `https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -63,7 +62,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${userPic}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -107,7 +106,7 @@ const createPost = (post) => {
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="${userPic}" alt="User Picture" />
+                      <img src="${post.userImage}" alt="User Picture" />
                     </a>
 
                     <span>Liked by
